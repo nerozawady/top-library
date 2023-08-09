43dc5aa1c16358c969771e7134b4bf99a78f9223
Nero Zawady <nerozawady@gmail.com>
2023-08-09 10:37:34 -0800

    Refactor into classes

--- main.js
+++ main.js
@@ -1,47 +1,81 @@
-// eslint-disable-next-line strict, lines-around-directive
-'use strict';
-
-// TODO - genres should be a collection of tags
-
-const library = [];
 const ETBody = document.querySelector('tbody');
 const EAddBook = document.querySelector('#add-book');
 const EForm = document.querySelector('form');
 const EFormCancel = document.querySelector('#form__cancel');
 const EFormAdd = document.querySelector('#form__add');
 const EFormEdit = document.querySelector('#form__edit');
+const EBooks = document.getElementsByName('book');
 
-function Book(id, title, author, date, pages, read) {
-  this.metadata = {
-    id,
-  };
-  this.title = title;
-  this.author = author;
-  this.date = date;
-  this.pages = pages;
-  this.read = read;
-}
+class BookList {
+  #books = [];
 
-function updateLibraryIds(removedBookId) {
-  for (let i = Number(removedBookId); i < library.length; i += 1) {
-    library[i].metadata.id = i;
+  addBook(title, author, date, pages, read) {
+    this.#books.push({
+      metadata: {
+        id: this.#books.length,
+      },
+      title,
+      author,
+      date,
+      pages,
+      read,
+    });
+  }
+
+  removeBook(id) {
+    id = Number(id);
+    this.#books.splice(id, 1);
+    for (let i = id; i < this.#books.length; i += 1) {
+      this.#books[i].metadata.id = i;
+    }
+  }
+
+  toggleRead(id) {
+    this.#books[id].read = !this.#books[id].read;
+  }
+
+  editBook(id, title, author, date, pages, read) {
+    id = Number(id);
+    this.#books[id] = {
+      metadata: {
+        id,
+      },
+      title,
+      author,
+      date,
+      pages,
+      read,
+    };
+  }
+
+  getBook(id) {
+    id = Number(id);
+    return this.#books.at(id);
   }
 }
 
+const library = new BookList();
+
 function addBookToDOM(book) {
   const ETRow = document.createElement('tr');
+  ETRow.setAttribute('name', 'book');
 
   const ERemove = document.createElement('button');
   ERemove.textContent = 'X';
   ERemove.addEventListener('click', event => {
-    const bookID = event.target.parentElement.getAttribute('data-id');
+    const bookID = Number(event.target.parentElement.getAttribute('data-id'));
     event.target.parentElement.remove();
-    library.splice(bookID, 1);
-    updateLibraryIds(bookID);
+    // event.target.parentElement.remove();
+    // EBooks.pop(bookID);
+
+    for (let i = bookID; i < EBooks.length; i += 1) {
+      EBooks[i].setAttribute('data-id', i);
+    }
+
+    library.removeBook(bookID);
   });
   ETRow.append(ERemove);
 
-  // TODO - implement feature: click "Edit" to get a form and edit the book, update library info
   const EEdit = document.createElement('button');
   EEdit.textContent = 'Edit';
   ETRow.append(EEdit);
@@ -58,7 +92,7 @@ function addBookToDOM(book) {
 
         ERead.addEventListener('change', event => {
           const bookId = event.target.parentElement.parentElement.getAttribute('data-id');
-          library[bookId].read = !library[bookId].read;
+          library.toggleRead(bookId);
         });
 
         ETD.append(ERead);
@@ -85,13 +119,6 @@ function showAddBookForm() {
 }
 
 EAddBook.addEventListener('click', showAddBookForm);
-// TODO
-// EEdit.addEventListener("click")
-
-// function showEditBookForm() {
-//   EFormEdit.classList.remove('hidden');
-//   EForm.classList.remove('hidden');
-// }
 
 EFormCancel.addEventListener('click', () => {
   EForm.classList.add('hidden');
@@ -147,17 +174,25 @@ EForm.addEventListener('submit', event => {
   event.preventDefault();
   const formData = new FormData(EForm);
 
-  const book = new Book(
-    library.length,
+  // const book = new Book(
+  //   library.length,
+  //   formData.get('title'),
+  //   formData.get('author'),
+  //   formData.get('date'),
+  //   formData.get('pages'),
+  //   formData.get('read') === 'on' ?? false
+  // );
+
+  // library.push(book);
+
+  library.addBook(
     formData.get('title'),
     formData.get('author'),
     formData.get('date'),
     formData.get('pages'),
-    formData.get('read') === 'on' ?? false
+    formData.get('read') === 'on'
   );
-
-  library.push(book);
-  addBookToDOM(library.at(-1));
+  addBookToDOM(library.getBook(-1));
 
   EForm.reset();
   EForm.classList.add('hidden');
@@ -166,12 +201,14 @@ EForm.addEventListener('submit', event => {
 function init() {
   EForm.reset();
 
-  library.push(new Book(0, 'Book 1', 'Adam', '2023-01-12', 481, false));
-  addBookToDOM(library.at(-1));
-  library.push(new Book(1, 'Book 2', 'Sam', '1523-07-27', 193, false));
-  addBookToDOM(library.at(-1));
-  library.push(new Book(2, 'Book 3', 'Derek', '1023-11-03', 231, true));
-  addBookToDOM(library.at(-1));
+  library.addBook('Book 1', 'Adam', '2023-01-12', 481, false);
+  addBookToDOM(library.getBook(-1));
+  library.addBook('Book 2', 'Sam', '1523-07-27', 193, false);
+  addBookToDOM(library.getBook(-1));
+  library.addBook('Book 3', 'Derek', '1023-11-03', 231, true);
+  addBookToDOM(library.getBook(-1));
+
+  // EBooks = document.querySelectorAll('tr[data-id]');
 }
 
 init();
